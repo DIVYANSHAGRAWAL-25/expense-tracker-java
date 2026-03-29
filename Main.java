222b3dca3e8fabd4bac9677ac96a7ca8db3a53b6
DIVYANSHAGRAWAL-25 <agrawaldivyansh0402@gmail.com>
2026-03-29 14:06:54 +0530

    Made better user prompts

--- Main.java
+++ Main.java
@@ -3,12 +3,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         expenseRecords myRecord = new expenseRecords();
+        System.out.println("Hi, I am your Helper in handling you expense");
         while(1 > 0){
-            System.out.println("below are the service we can provide");
-            System.out.println("\na. record my expense");
-            System.out.println("b. show my expense");
-            System.out.println("c. my total expenditure");
-            System.out.println("d. take an exit?");
+            System.out.println("Please tell what do you want? ");
+            System.out.println("\nA. record my expense");
+            System.out.println("B. show my expense");
+            System.out.println("C. my total expenditure");
+            System.out.println("D. done? if yes please press D");
             System.out.println("\n");
             System.out.print("click A or B or C or D in capital only ");
             System.out.println("\n");
@@ -17,15 +18,17 @@ public class Main {
             switch (choice) {
                 case 'A':
                     // asking what is the thing that you spent on?
-                    System.out.print("On what you did you spent? ");
+                    System.out.print("On what you did you spent? ==> ");
                     String s = sc.nextLine();
                     // how much did it cost?
-                    System.out.print("how much did you spent? type a number ");
+                    System.out.print("how much did you spent? (type the amount in number) ==> ");
                     double a = sc.nextDouble();
                     sc.nextLine();
                     // what group should we put into?
-                    System.out.print("which group does it fit in? ");
+                    System.out.print("In what type should I update this ==> ");
                     String g = sc.nextLine();
+                    System.out.print("\n");
+                    System.out.print("Do you want to continue? feel free");
                     System.out.print("\n");
                     myRecord.recordAnExpense(new expense(a, s, g));
                     break;
